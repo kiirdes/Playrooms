4fb41b76aa17797d3fa04815b966cb4cc6c11a58
kiirdes <caquilalacedric@gmail.com>
2024-03-10 09:10:41 +0800

    added '01_oracle_quest.dart'

---
+++ 01_oracle_ques.dart
@@ -0,0 +1,16 @@
+List<String> allCombos = [];
+
+void GetAllCombos(String str) {
+  // 1. get one element
+  // 2. shuffle the rest
+  // ???
+  // 5. profit
+}
+
+void main() {
+  String code = 'abcde';
+
+  print(allCombos);
+  GetAllCombos(code);
+  print(allCombos);
+}
