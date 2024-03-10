8f7e6f47a480ba6a4208374d4b5f439b3383eb5a
kiirdes <caquilalacedric@gmail.com>
2024-03-10 11:14:11 +0800

    finished function to get all permutations of a string

--- 01_oracle_ques.dart
+++ 01_oracle_ques.dart
@@ -1,16 +1,53 @@
-List<String> allCombos = [];
+/*
+SplitString(String str, int idx)
+  - separates one string into a | bcd...n , where
+  where a is the str[idx] and b is str excluding str[idx].
+*/
+(String, String) SplitString(String str, int idx) {
+  String remaining = '';
 
-void GetAllCombos(String str) {
-  // 1. get one element
-  // 2. shuffle the rest
-  // ???
-  // 5. profit
+  for (int i = 0; i < str.length; i++) {
+    if (idx == i) {
+      continue;
+    }
+
+    remaining += str[i];
+  }
+
+  return (str[idx], remaining);
+}
+
+/*
+GetAllPermutations(String str)
+  - recursive function which returns all possible permutations
+  of string str.
+*/
+Set<String> GetAllPermutations(String str) {
+  Set<String> allPermutations = new Set<String>();
+
+  if (str.length <= 2) {
+    allPermutations.add(str[0] + str[1]);
+    allPermutations.add(str[1] + str[0]);
+    return allPermutations;
+  }
+
+  for (int i = 0; i < str.length; i++) {
+    String curr, remaining;
+    (curr, remaining) = SplitString(str, i);
+
+    GetAllPermutations(remaining).forEach((element) {
+      allPermutations.add(curr + element);
+    });
+  }
+
+  return allPermutations;
 }
 
 void main() {
-  String code = 'abcde';
+  String code = 'abb';
+  Set<String> allPermutations = new Set<String>();
 
-  print(allCombos);
-  GetAllCombos(code);
-  print(allCombos);
+  print("Code: ${code}");
+  allPermutations = GetAllPermutations(code);
+  print("All Permutations: ${allPermutations}");
 }
