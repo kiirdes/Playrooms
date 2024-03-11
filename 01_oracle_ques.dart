377c073581e1f65a269e7fce30fe640c8f7b9fb2
kiirdes <caquilalacedric@gmail.com>
2024-03-11 23:56:02 +0800

    added key-value converter

--- 01_oracle_ques.dart
+++ 01_oracle_ques.dart
@@ -43,11 +43,75 @@ Set<String> GetAllPermutations(String str) {
   return allPermutations;
 }
 
+/*
+GetCharValue(List<int> key, String ch)
+  - returns the value of the character based on key
+*/
+int GetCharValue(List<int> key, String ch) {
+  int idx = ch.toLowerCase().codeUnitAt(0) - 'a'.codeUnitAt(0);
+
+  if (26 < idx) {
+    return -1;
+  }
+
+  return key[idx];
+}
+
+/*
+GetDecryptScore(List<int> key, String str) {
+  - returns the total score of a char based on its difference from
+  its neighboring chars.
+  - score[i] = abs(x[i-1]-x[i])+abs(x[i]-x[i+1])
+}
+*/
+int GetDecryptScore(List<int> key, String str) {
+  if (str.length == 1) {
+    return 0;
+  }
+
+  int score = 0;
+
+  for (int i = 0; i < str.length; i++) {
+    if (0 != i) {}
+  }
+
+  return score;
+}
+
 void main() {
-  String code = 'abb';
+  String code = 'a';
+  List<int> key = [
+    2,
+    6,
+    5,
+    8,
+    4,
+    3,
+    6,
+    5,
+    4,
+    6,
+    5,
+    4,
+    5,
+    3,
+    4,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    6,
+    5,
+    3,
+    2
+  ];
   Set<String> allPermutations = new Set<String>();
 
-  print("Code: ${code}");
+  print("Code: ${code}. Key length: ${key.length}");
   allPermutations = GetAllPermutations(code);
   print("All Permutations: ${allPermutations}");
+  print("Value of ${code}: ${GetCharValue(key, code[0])}");
 }
